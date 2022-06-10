895f16babdeb3ad27321dee493e675ee81f57951
lliang <lari.liang@gmail.com>
2022-06-09 20:09:19 -0500

    access minio

---
+++ src/main/java/demo.java
@@ -0,0 +1,39 @@
+import io.minio.MinioClient;
+import io.minio.ObjectStat;
+import io.minio.errors.MinioException;
+
+public class demo {
+    public static void main(String[] args) throws Exception {
+        try {
+            //第一步：创建Minio客户端进行连接，共有三个参数 ip:端口，账号，密码
+            MinioClient minioClient = new MinioClient("http://127.0.0.1:9000", "minioadmin", "minioadmin");
+
+            //第二步：bucket表示的是文件夹，检查文件夹是否已经存在
+            boolean isExist = minioClient.bucketExists("filedir");
+            if(!isExist) {
+                //如果filedir文件夹不存在，则创建一个名为filedir的文件夹
+                minioClient.makeBucket("filedir");
+            }
+
+            /**
+             * 第三步：使用putObject()上传一个文件到文件夹中
+             * 第一个参数：文件夹；第二个参数：定义的文件名；第三个参数：需要上传文件的文件路径
+             */
+            minioClient.putObject("filedir","sample.txt", "/Users/leiliang/Data/origSample.txt");
+
+            //第四步：调用statObject()来判断对象(文件)是否存在
+            ObjectStat objectStat=minioClient.statObject("filedir", "sample.txt");
+            if(objectStat!=null){
+                System.out.println("存在");
+            }
+
+            /**
+             * 第五步：使用getObject获取一个文件
+             * 第一个参数：文件夹；第二个参数：要获取的文件名；第三个参数：要写入的文件路径
+             */
+            minioClient.getObject("filedir", "sample.txt", "/Users/leiliang/Data/newSample.txt");
+        } catch(MinioException e) {
+            System.out.println("使用Minio客户端处理文件出现异常: " + e);
+        }
+    }
+}
